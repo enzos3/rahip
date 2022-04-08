4bceda5dc12c3bc17f4e3bd46b142bb8f510e9ad
enzos3 <enzos3397@gmail.com>
2022-04-08 15:37:07 -0300

    Design Web Site

--- Rahip-web/scripts/formulario-contacto.js
+++ Rahip-web/scripts/formulario-contacto.js
@@ -8,7 +8,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/rahip.estudio@gmail.com", {
+    fetch("https://formsubmit.co/ajax/enzos3397@gmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
